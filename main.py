4423f0cb21e83ddb288d6fc5c0b1d6561df6cae5
Amlynar <amlynar93@gmail.com>
2026-09-09 20:12:50 -0400

    Changed the main print message to a new message.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # from src.common.models import Project
 
 def main():
-    print("This is the main function. You can create a Project instance here.")
+    print("This is a new message")
     # project = Project(id=1, name="Sample Project", repo_url="https://github.com/example/repo")
     # print(f"Project Name: {project.name}")
     # print(f"Repository URL: {project.repo_url}")
