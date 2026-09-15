145dbcce8c09c5053e2d81747c75f0e27ebb1157
Amlynar <amlynar93@gmail.com>
2026-09-14 21:44:21 -0400

    Test injector

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from injector import Injector
+from src.common.repositories.catalog_repo import CatalogRepository
 from src.application.pocket_repo_app import PocketRepoApp
 # from src.application.dev_app import DevApp
 
 def main():
     injector = Injector()
     app = injector.get(PocketRepoApp)
+
+    app.catalog_repo.load_catalog()
+    projects = app.catalog_repo.get_all_projects()
+    print(f"Catalogs loaded: {len(projects)}")
+
+
+    test = injector.get(CatalogRepository)
+    projects2 = test.get_all_projects()
+    print(f"Catalogs loaded from test: {len(projects2)}")
+
     app.run()
     pass
 
