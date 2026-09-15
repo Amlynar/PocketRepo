9943458bd0e8be12bc4acb2f1e7a011de803443f
Amlynar <amlynar93@gmail.com>
2026-09-14 21:30:37 -0400

    add debug log

--- src/application/pocket_repo_app.py
+++ src/application/pocket_repo_app.py
@@ -15,5 +15,6 @@ class PocketRepoApp:
     def run(self):
 
         self.catalog_repo.load_catalog()
+        print(f"Loaded {len(self.catalog_repo.get_all_projects())} projects from catalog.")
         self.project_list_controller.view.present("fullscreen")
             
