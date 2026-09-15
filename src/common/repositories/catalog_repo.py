5cb2d1e74a67ea8b611b23e20010faee426dc53f
Amlynar <amlynar93@gmail.com>
2026-09-14 20:12:10 -0400

    Refactor CatalogRepository to hold Dict of Projects instead of list

--- src/common/repositories/catalog_repo.py
+++ src/common/repositories/catalog_repo.py
@@ -1,30 +1,31 @@
 import json
-from typing import List
+from typing import Dict, List
 from injector import singleton
 
 from src.common.models.project import Project
 
+
 CATALOG_FILE_PATH = "./assets/catalog.json"
+
 
 @singleton
 class CatalogRepository:
     def __init__(self):
-        self._catalogs: list[Project] = []
+        self._catalogs: Dict[str, Project] = {}
 
     def load_catalog(self) -> None:
         with open(CATALOG_FILE_PATH, 'r') as f:
             data = json.load(f)
-            self._catalogs = [Project.from_dict(c) for c in data.get("projects", [])]
+            self._catalogs = {project.id: project for project in (Project.from_dict(c) for c in data.get("projects", []))}
 
-    def get_catalogs(self) -> list[Project]:
-        return self._catalogs
+    def get_catalogs(self) -> List[Project]:
+        return list(self._catalogs.values())
 
     def project_exists(self, project_id: str) -> bool:
-        return any(project.id == project_id for project in self._catalogs)
+        return project_id in self._catalogs
 
     def get_project_by_id(self, project_id: str) -> Project:
-        for project in self._catalogs:
-            if project.id == project_id:
-                return project
-        print(f"Project with ID '{project_id}' not found in the catalog.")
-        raise ValueError(f"Project with ID '{project_id}' not found in the catalog.")
+        if project_id not in self._catalogs:
+            print(f"Project with ID '{project_id}' not found in the catalog.")
+            raise ValueError(f"Project with ID '{project_id}' not found in the catalog.")
+        return self._catalogs[project_id]
