52ecf8d3571f7d47b61ffea9395717bd9065bc8e
Amlynar <amlynar93@gmail.com>
2026-09-14 21:02:36 -0400

    Set backroung color to white

--- src/screens/project_list/project_list_view.py
+++ src/screens/project_list/project_list_view.py
@@ -4,6 +4,7 @@ import ui
 class ProjectListView(ui.View):
     def __init__(self):
         super().__init__()
+        self.background_color = 'white'
         self.model = ProjectListModel()
         self.table_view = ui.TableView()
         self.flex = 'LRTB'
