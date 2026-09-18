a73feabae189266631854ef2c0d560d080d63ddd
Amlynar <amlynar93@gmail.com>
2026-09-17 22:06:43 -0400

    Test first screen approach

--- src/application/pocket_repo_app.py
+++ src/application/pocket_repo_app.py
@@ -1,5 +1,6 @@
 from injector import Injector, inject
 
+from src.screens.base.base_controller import PRBaseController
 from src.navigation.navigation_route import PRNavigation, PRNavigationRoute, ProjectListScreenRoute
 from src.navigation.navigation_manager import NavigationManager
 from src.screens.project_list.project_list_controller import ProjectListController
@@ -16,21 +17,27 @@ class PocketRepoApp:
         self.navigation = PRNavigation()
         self.navigation.navigate = self.navigate
 
+        self.current_controller = None
+
     def run(self):
 
+        self.current_controller = self.get_and_decorate_controller(ProjectListScreenRoute())
+        self.navigation_manager.setup_navigation(self.current_controller.view)
         self.navigation_manager.present()
 
-        self.navigation.navigate(ProjectListScreenRoute())
+        # self.navigation.navigate(ProjectListScreenRoute())
         # self.catalog_repo.load_catalog()
         # print(f"Loaded {len(self.catalog_repo.get_all_projects())} projects from catalog.")
         # self.project_list_controller.view.present("fullscreen")
 
 
     def navigate(self, route: PRNavigationRoute):
-        # self.navigation_manager.navigate(route)
-        controller = route.to_controller(self.injector)
-        controller.navigation = self.navigation
-
+        controller = self.get_and_decorate_controller(route)
         self.navigation_manager.push_screen(controller.view)
+
+    def get_and_decorate_controller(self, route: PRNavigationRoute) -> PRBaseController:
+        self.current_controller = route.to_controller(self.injector)
+        self.current_controller.navigation = self.navigation
+        return self.current_controller
 
 
