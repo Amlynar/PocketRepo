18feadff79778814670ad7c07093bb18e016f2bf
Amlynar <amlynar93@gmail.com>
2026-09-18 16:28:35 -0400

    change wh

--- src/ui/project_list/project_list_view.py
+++ src/ui/project_list/project_list_view.py
@@ -20,7 +20,7 @@ class ProjectListView(PRBaseScreen):
         self.label.text = "Text Label"
         self.label.background_color = "black"
         self.label.text_color = "white"
-        self.label.flex = 'WH'
+        self.label.flex = "WH"
         self.label.alignment = 1
         self.root_view.add_subview(self.label)
 
