df23efb62826d6a6de8518f7c98e5db11ac0437a
Amlynar <amlynar93@gmail.com>
2026-09-18 10:10:50 -0400

    test move show spinner to init block

--- src/ui/project_list/project_list_controller.py
+++ src/ui/project_list/project_list_controller.py
@@ -24,7 +24,9 @@ class ProjectListController(PRBaseController):
         # self.model.projects = self.catalog_repository.get_all_projects()
         # self.view.update_ui(self.model)
 
+        self.view.show_loading_spinner()
+
     def on_screen_loaded(self):
         print("ProjectListController: Screen loaded.")
-        self.view.show_loading_spinner()
+        # self.view.show_loading_spinner()
         pass
