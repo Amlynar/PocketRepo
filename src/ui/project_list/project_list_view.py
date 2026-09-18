ce0528750109cd37c9b602c3b59c0c309a24fcde
Amlynar <amlynar93@gmail.com>
2026-09-18 16:44:48 -0400

    reintroduce flex

--- src/ui/project_list/project_list_view.py
+++ src/ui/project_list/project_list_view.py
@@ -20,9 +20,9 @@ class ProjectListView(PRBaseScreen):
         self.label.text = "Text Label"
         self.label.background_color = "black"
         self.label.text_color = "white"
-        # self.label.flex = "WH"
-        self.label.height = 1000.0
-        self.label.width = 1000.0
+        self.label.flex = "WH"
+        # self.label.height = 1000.0
+        # self.label.width = 1000.0
         self.label.alignment = 1
         self.root_view.add_subview(self.label)
 
