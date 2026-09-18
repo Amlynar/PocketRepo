b5adb1cf9dbfd068fa18112aa25898b6c2bb3f25
Amlynar <amlynar93@gmail.com>
2026-09-18 10:06:17 -0400

    Add more debug logging

--- src/ui/base/base_screen.py
+++ src/ui/base/base_screen.py
@@ -10,18 +10,19 @@ class PRBaseScreen(ui.View):
         super().__init__()
 
     def show_loading_spinner(self):
+        print("Showing loading spinner...")
         self.spinner = ui.ActivityIndicator()
-        
+        print("ActivityIndicator created.")
         # Choose style: 'gray', 'white', or 'white_large'
         self.spinner.style = ui.ACTIVITY_INDICATOR_STYLE_WHITE_LARGE
         self.spinner.color = '#333333' # Dark gray spinner color
-        
+        print("set styles")
         # Ensure it stays on screen when initialized
         self.spinner.hides_when_stopped = True
-        
+        print("hides when stopped")
         # Add the spinner to this view hierarchy
         self.add_subview(self.spinner)
-        
+        print("added subview")
         # Start the spinning animation automatically
         self.spinner.start()
         print("Loading spinner shown.")
