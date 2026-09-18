f9d745a62a9a67d883b6130d4d8e72804650b405
Amlynar <amlynar93@gmail.com>
2026-09-18 16:23:05 -0400

    Cleanup and set flex to nav view

--- src/navigation/navigation_manager.py
+++ src/navigation/navigation_manager.py
@@ -7,6 +7,7 @@ class NavigationManager:
 
         self.nav_view = ui.NavigationView(self.root_view)
         self.nav_view.background_color = 'white'
+        self.nav_view.flex = 'WH'
 
     def push_screen(self, next_view: ui.View):
         self.nav_view.push_view(next_view)
@@ -16,10 +17,4 @@ class NavigationManager:
         
     def present(self, style='fullscreen'):
         self.nav_view.present(style)
-
-    def setup_navigation(self, view: ui.View):
-        self.root_view = view
-
-        self.nav_view = ui.NavigationView(self.root_view)
-        self.nav_view.background_color = 'white'
         
