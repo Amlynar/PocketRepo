40a0a8d346c0fd84180d1875cb7a4da116b0c6fd
Amlynar <amlynar93@gmail.com>
2026-09-17 21:58:15 -0400

    Test first screen

--- src/navigation/navigation_manager.py
+++ src/navigation/navigation_manager.py
@@ -3,15 +3,13 @@ import ui
 class NavigationManager:
 
     def __init__(self):
-        self.root_view = ui.View()
-        self.root_view.name = "PocketRepo"
-        self.root_view.background_color = 'white'
-
-        self.nav_view = ui.NavigationView(self.root_view)
+        self.root_view = None
 
     def push_screen(self, next_view: ui.View):
-        """Pushes a new ui.View onto the navigation stack."""
-        self.nav_view.push_view(next_view)
+        if not self.root_view:
+            self.setup_navigation(next_view)
+        else:
+            self.nav_view.push_view(next_view)
         
     def pop_screen(self):
         """Pops the top view off the stack (returns to the previous screen)."""
@@ -20,4 +18,11 @@ class NavigationManager:
     def present(self, style='fullscreen'):
         """Displays the navigation container on screen."""
         self.nav_view.present(style)
+
+    def setup_navigation(self, view: ui.View):
+        self.root_view = view
+        self.root_view.name = "PocketRepo"
+        self.root_view.background_color = 'white'
+
+        self.nav_view = ui.NavigationView(self.root_view)
         
