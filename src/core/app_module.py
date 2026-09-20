a79c4b347a32b7a1493c46c3b65e3d4cc48a6e33
Amlynar <amlynar93@gmail.com>
2026-09-20 15:31:43 -0400

    Test navigation background color

--- src/core/app_module.py
+++ src/core/app_module.py
@@ -11,7 +11,8 @@ class AppModule(Module):
     def provide_theme(self) -> PYITheme:
         return (
             PYIThemeBuilder()
-            .set_screen_background_color('red')
+            # .set_screen_background_color('red')
+            .set_navigation_bar_background_color('red')
             .build()
         )
     
