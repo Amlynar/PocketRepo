cc7716c483e68cbea3e34d64b5e252eb8fc4f960
Amlynar <amlynar93@gmail.com>
2026-09-22 09:43:53 -0400

    Remove views in init

--- src/screens/project_details/project_details_view.py
+++ src/screens/project_details/project_details_view.py
@@ -14,35 +14,6 @@ class ProjectDetailsView(PYIScreen):
         super().__init__(theme=theme)
         self.theme = theme
 
-        self.title_label = ui.Label()
-        PYILabelStyle.normal(self.title_label, self.theme)
-        self.title_label.text = 'My Title'
-        # self.title_label.font = ('<system-bold>', 24)
-        self.title_label.alignment = ui.ALIGN_CENTER
-        # self.title_label.text_color = '#1c1c1e'
-        
-        self.desc_label = ui.Label()
-        PYILabelStyle.normal(self.title_label, self.theme)
-        self.desc_label.text = 'This dashboard displays your upcoming tasks. Select an item below to view more detailed insights or to complete it.'
-        # self.desc_label.font = ('<system>', 14)
-        self.desc_label.alignment = ui.ALIGN_CENTER
-        # self.desc_label.text_color = '#3a3a3c'
-        self.desc_label.number_of_lines = 0  # Allows multi-line wrapping
-        
-        # 3. TableView
-        self.table_view = ui.TableView()
-        PYITableViewStyle.normal(self.table_view, self.theme)
-        # self.table_view.background_color = '#ffffff'
-        # self.table_view.corner_radius = 8
-
-        for v in  (
-            self.title_label,
-            self.desc_label,
-            self.table_view
-        ):
-            self.root_view.add_subview(v)
-
-
     def layout(self):
         super().layout()
 
@@ -91,4 +62,3 @@ class ProjectDetailsView(PYIScreen):
             self.table_view
         ):
             self.root_view.add_subview(v)
-            
