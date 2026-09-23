8c2a28206095a362226ceb1d92b0f9f8b399a0d8
Amlynar <amlynar93@gmail.com>
2026-09-22 21:38:07 -0400

    Remove positive and negative buttons from simple alert

--- src/framework/alert/alert_dialog.py
+++ src/framework/alert/alert_dialog.py
@@ -3,6 +3,6 @@ import console
 class PYIAlert:
 
     @staticmethod
-    def simple_alert(title: str, message: str, positive_button: str="Ok", negative_button: str="Cancel"):
-        return console.alert(title,message,positive_button,negative_button)
+    def simple_alert(title: str, message: str):
+        return console.alert(title,message)
     
